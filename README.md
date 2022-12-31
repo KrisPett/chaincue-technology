52db1c7389e2eb1facb89fc08cf0d1cafa52c6ca
KQT3 <kristoffer@email.com>
2022-12-31 12:01:43 +0100

    commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ```
 https://kqt3-psychic-garbanzo-wxjjqq9jpqgf55vv.github.dev/
+
 https://github.dev/bketelsen/spotlight
 https://github.com/bradtraversy/next-crash-course
 ```
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="apple-touch-icon" sizes="76x76" href="/static/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
+        <link rel="icon" type="image/ico" sizes="32x32" href="/static/favicon.ico"/>
         <link rel="manifest" href="/static/site.webmanifest" />
         <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#000000" />
