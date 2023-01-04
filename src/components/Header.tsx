073be3165c365b9c39bd800eb5856088849edf1c
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-01-04 19:54:11 +0100

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,46 @@
 import React from 'react';
 import Image from 'next/image'
 import ChainLogoWhite from "../assets/image/chaincue-logo-crop1-white.png";
-import SocialIcon from "./SocialIcon";
+import ChainLogoDark from "../assets/image/chaincue-logo-crop1-dark.png";
 import IconHeader from "./IconHeader";
-import Drawer from "./Drawer";
-import ThemeSwitch2 from "./ThemeSwitch2";
 import ThemeSwitch1 from "./ThemeSwitch1";
+import {useTheme} from "next-themes";
+import Link from "next/link";
 
 const Header = () => {
+  const {theme, setTheme, resolvedTheme} = useTheme()
+
   return (
     <header className={"bg-zinc-50 dark:bg-zinc-600"}>
       <div className={"flex items-center justify-between py-3"}>
         <div className={"ml-3"}>
           <a href={"#"}>
-            {/*<img src={ChainLogoWhite} alt="" width={300} height={300}/>*/}
-            <Image src={ChainLogoWhite} alt="" width={300} height={300} className="min-w-full"/>
+            {theme === "dark" ?
+              <Image src={ChainLogoDark} alt="" width={300} height={300} className="min-w-full" priority={true}/>
+              : <Image src={ChainLogoWhite} alt="" width={300} height={300} className="min-w-full" priority={true}/>}
           </a>
         </div>
-        <div >
-          {/*<ul className={"flex space-x-1"}>*/}
-          {/*  <li>*/}
-          {/*    <Link href="/">Projects</Link>*/}
-          {/*  </li>*/}
-          {/*  <li>*/}
-          {/*    <Link href="/about">About</Link>*/}
-          {/*  </li>*/}
-          {/*  <li>*/}
-          {/*    <Link href="/contact">Contact</Link>*/}
-          {/*  </li>*/}
-          {/*</ul>*/}
+        <div>
+          <ul className={"flex space-x-1"}>
+            <li>
+              <Link href="/">Projects</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
         </div>
         <div className="flex flex-wrap justify-center gap-1 mr-2">
+          <div className={" flex mr-2"}>
+            <ThemeSwitch1/>
+          </div>
           <IconHeader kind={"github"} href={"#"}/>
           <IconHeader kind={"gitlab"} href={"#"}/>
           <IconHeader kind={"twitter"} href={"#"}/>
           <IconHeader kind={"linkedin"} href={"#"}/>
-          <ThemeSwitch1/>
         </div>
       </div>
       {/*<div className="btn-group">*/}
@@ -45,7 +50,7 @@ const Header = () => {
       {/*  <button className="btn btn-outline btn-warning">Warning</button>*/}
       {/*  <button className="btn btn-outline btn-error">Error</button>*/}
       {/*</div>*/}
-      <Drawer/>
+      {/*<Drawer/>*/}
     </header>
   )
 }
--- src/components/IconHeader.tsx
+++ src/components/IconHeader.tsx
@@ -27,13 +27,26 @@ const IconHeader = ({kind, href}: ISocialIcon) => {
 
   return (
     <div>
+
+      {/*<button className="btn btn-circle">*/}
+      {/*  <SocialSvg className={"fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-4 w-4"}/>*/}
+      {/*</button>*/}
       <span className="sr-only">{kind}</span>
-      <button type="button"
-              className="text-blue-700 border border-blue-700 hover:bg-zinc-700 hover:text-white focus:ring-4
-              focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex
-              items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
-        <SocialSvg className={"fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-2 w-2"}/>
+      <button className="btn btn-circle btn-outline text-blue-700 border border-blue-700 hover:bg-zinc-100 hover:text-white focus:ring-2
+              focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center
+              dark:hover:bg-zinc-700 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800
+              hover:text-blue-500 dark:hover:text-blue-400
+              "
+      >
+        <SocialSvg className={"fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-4 w-4"}/>
       </button>
+
+      {/*<button type="button"*/}
+      {/*        className="text-blue-700 border border-blue-700 hover:bg-zinc-700 hover:text-white focus:ring-4*/}
+      {/*        focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex*/}
+      {/*        items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">*/}
+      {/*  <SocialSvg className={"fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-4 w-4"}/>*/}
+      {/*</button>*/}
     </div>
   );
 };
