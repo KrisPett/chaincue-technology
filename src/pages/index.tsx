27f84c67065d141026d6bb05c20ff989e6629794
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-01-05 22:14:55 +0100

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <main>
         <div>
           <h1 className="text-3xl font-bold">
-           KQT3
+           KQT33
           </h1>
           <select value={theme} onChange={e => setTheme(e.target.value)}>
             <option value="system">System</option>
