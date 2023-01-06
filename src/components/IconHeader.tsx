640f5809b9ddbe893223bb6b71403402229035bd
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-01-06 17:12:34 +0100

    commit

--- src/components/IconHeader.tsx
+++ src/components/IconHeader.tsx
@@ -29,9 +29,9 @@ const IconHeader = ({kind, href, tooltip}: ISocialIcon) => {
   return (
     <div className={"group tooltip tooltip-bottom"} data-tip={tooltip}>
       <span className="sr-only">{kind}</span>
-      <button className=" hover:fill-current btn btn-circle btn-outline text-orange-700 border border-orange-700 hover:bg-zinc-100 hover:text-white focus:ring-2
+      <button className=" hover:fill-current btn btn-circle btn-outline text-orange-700 border border-orange-700 hover:border-orange-700 hover:bg-zinc-100 hover:text-white focus:ring-2
               focus:outline-none focus:ring-orange-500 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center
-              dark:hover:bg-zinc-700 dark:border-orange-500 dark:text-orange-500 dark:hover:text-white dark:focus:ring-orange-800">
+              dark:hover:bg-zinc-700 dark:border-orange-500 dark:text-orange-500 dark:hover:text-white dark:focus:ring-orange-400">
         <SocialSvg
           className={`group-hover:text-orange-600 dark:group-hover:text-orange-400 hover:fill-current fill-current text-gray-700 
           hover:text-orange-500 dark:text-gray-200 dark:hover:text-orange-400 h-4 w-4`}/>
