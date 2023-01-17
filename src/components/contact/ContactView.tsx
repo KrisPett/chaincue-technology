f3fca31cfa6c8067690ed6d274daa243b5647699
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-01-17 20:54:39 +0100

    commit

--- src/components/contact/ContactView.tsx
+++ src/components/contact/ContactView.tsx
@@ -1,13 +1,35 @@
 import React, {useState} from 'react';
 import dynamic from 'next/dynamic';
 
-const ContactView = () => {
-  const [text, setText] = useState()
+interface IForm {
+  name: string;
+  email: string;
+  message: string;
+}
 
-  const GlobeSpin = dynamic(() => import('./GlobeSpin'), {
-    ssr: false,
-    loading: () => <div className={"flex items-center justify-center w-full"}>{/*<Loading/>*/}</div>,
-  })
+const GlobeSpin = dynamic(() => import('./GlobeSpin'), {
+  ssr: false,
+  loading: () => <div className={"flex items-center justify-center w-full"}>{/*<Loading/>*/}</div>,
+})
+
+const ContactView = () => {
+  const [form, setForm] = useState<IForm>({name: '', email: '', message: ''})
+
+  const onTextName = (value: string) => {
+    setForm({...form, name: value})
+  }
+
+  const onTextEmail = (value: string) => {
+    setForm({...form, email: value})
+  }
+
+  const onTextMessage = (value: string) => {
+    setForm({...form, message: value})
+  }
+
+  const onClickButton = () => {
+    console.log(form)
+  }
 
   return (
     <div className={"flex flex-row"}>
@@ -20,6 +42,7 @@ const ContactView = () => {
               <span className="label-text dark:text-gray-100">Name</span>
             </label>
             <input type="text" placeholder="Type here"
+                   onChange={e => onTextName(e.target.value)}
                    className="input input-bordered focus-visible:border-0 focus-visible:ring-0 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 dark:focus-visible:ring-offset-orange-600
                     text-gray-800 bg-zinc-200 dark:text-gray-100 dark:bg-zinc-600 dark:placeholder-neutral-content/70"/>
           </div>
@@ -28,6 +51,7 @@ const ContactView = () => {
               <span className="label-text dark:text-gray-100">Your email*</span>
             </label>
             <input type="text" placeholder="Type here"
+                   onChange={e => onTextEmail(e.target.value)}
                    className="input input-bordered focus-visible:border-0 focus-visible:ring-0 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 dark:focus-visible:ring-offset-orange-600
                     text-gray-800 bg-zinc-200 dark:text-gray-100 dark:bg-zinc-600 dark:placeholder-neutral-content/70"/>
           </div>
@@ -36,12 +60,14 @@ const ContactView = () => {
               <span className="label-text dark:text-gray-100">Message*</span>
             </label>
             <textarea
+              onChange={e => onTextMessage(e.target.value)}
               className="textarea textarea-bordered h-40
               focus-visible:border-0 focus-visible:ring-0 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 dark:focus-visible:ring-offset-orange-600
               text-gray-800 bg-zinc-200 dark:text-gray-100 dark:bg-zinc-600 dark:placeholder-neutral-content/70"
               placeholder="Type your message"></textarea>
           </div>
           <button
+            onClick={() => onClickButton()}
             className="btn relative inline-flex items-center justify-center my-3 xxs:w-full md:w-fit p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-tr from-zinc-300 to-orange-300 group-hover:to-orange-400 hover:text-gray-900 text-gray-800 dark:bg-gradient-to-tr dark:from-zinc-500 dark:to-orange-600 dark:group-hover:to-orange-400 dark:hover:text-white dark:text-white ">
             <span
               className="relative px-5 py-2.5 xxs:w-full md:w-fit transition-all ease-in duration-75 bg-zinc-200 dark:bg-zinc-700 rounded-md group-hover:bg-opacity-0">
