fa4e7ad1d40ffb885d77839a3897a96947cce571
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-01-17 22:11:16 +0100

    commit

--- src/components/contact/GlobeSpin.tsx
+++ src/components/contact/GlobeSpin.tsx
@@ -1,11 +1,19 @@
 import React, {useCallback, useEffect, useRef, useState} from "react";
 import Globe, {GlobeMethods} from "react-globe.gl";
 import {useMedia} from "use-media";
+import * as THREE from 'three';
+
+const globeMaterial = new THREE.MeshPhongMaterial();
+globeMaterial.bumpScale = 20;
+new THREE.TextureLoader().load('//unpkg.com/three-globe/example/img/earth-water.png', texture => {
+  globeMaterial.specularMap = texture;
+  globeMaterial.specular = new THREE.Color('grey');
+  globeMaterial.shininess = 50;
+});
 
 const GlobeSpin = () => {
   const globeEl = useRef<GlobeMethods | undefined>(undefined);
   const [dimensions, setDimensions] = useState({width: 0, height: 0})
-
   const handleResize = useCallback(() => setDimensions({width: window.innerWidth, height: window.innerHeight}), [])
   const isMobile = useMedia({maxWidth: "1024px"});
 
@@ -23,16 +31,29 @@ const GlobeSpin = () => {
     }
   }, [isMobile])
 
+  useEffect(() => {
+    setTimeout(() => {
+      // @ts-ignore
+      const directionalLight = globeEl.current.scene().children.find(obj3d => obj3d.type === 'DirectionalLight');
+      directionalLight && directionalLight.position.set(1, 1, 1);
+    });
+  }, []);
+
   return (
     <Globe
+      globeMaterial={globeMaterial}
       ref={globeEl}
       width={dimensions.width / 2}
       height={dimensions.height / 2}
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       backgroundColor={"rgba(0,0,0,0)"}
       arcDashAnimateTime={1}
       arcColor={() => "rgba(255, 255, 255, 0.5)"}
+      // globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+      // bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+      // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
     />
   )
 }
