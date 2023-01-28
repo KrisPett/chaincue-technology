cce14bbe8103dbc5b3bd936d10fdf60d2e85087e
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-01-28 17:23:34 +0100

    commit

--- src/components/projects/ProjectsView.tsx
+++ src/components/projects/ProjectsView.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from "../home/Button";
 import Image from "next/image";
-import chainmusic from "../../assets/image/chanmusic.png";
 import alter from "../../assets/image/26193889.png";
 import {useRouter} from 'next/navigation';
 import {links} from "../common/Links";
@@ -13,7 +12,7 @@ const ProjectsView = () => {
       <div className={"justify-center flex flex-col space-y-5 max-w-7xl max-w-full"}>
         <div className="card card-side shadow-xl shadow-zinc-500/40 m-10 rounded-lg h-96 bg-zinc-300 dark:bg-zinc-700">
           <div className={"xxs:hidden md:block"}>
-            <Image className={"rounded-l-lg w-full sm:max-w-prose h-full"} src={chainmusic} alt="chainmusic"/>
+            <Image className={"rounded-l-lg w-full sm:max-w-prose h-full"}  width={"100"} height={"100"} src={"https://images.chaincuet.com/logos/images/chanmusic.png"} alt="chainmusic"/>
           </div>
           <div className="card-body">
             <h2 className="card-title">ChainMusic</h2>
