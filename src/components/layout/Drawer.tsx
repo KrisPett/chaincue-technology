9f3a0c2c419f8d40c6cc042c183cb4c09bd02b08
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-09-16 09:59:05 +0200

    commit

--- src/components/layout/Drawer.tsx
+++ src/components/layout/Drawer.tsx
@@ -1,4 +1,5 @@
-yar
+import {useState} from "react";
+
 const Drawer = () => {
 
   const [isOpen, setIsOpen] = useState(false);
@@ -15,20 +16,21 @@ const Drawer = () => {
   const fullPage = isOpen ? 'fixed h-screen w-full z-20' : hide;
 
   return (
-    <>
-      <div className="btn btn-primary" onClick={clickHandler}>
-        open menu
-      </div>
-      <div className={`rounded-lg drawer drawer-end fixed h-screen w-full ${fullPage} `}>
-        <input id="my-drawer" type="checkbox" onChange={() => console.log("")} className="drawer-toggle" checked={isOpen}/>
-        <div className="drawer-side">
-          <label form="my-drawer" className="drawer-overlay" onClick={clickHandler}></label>
-          <ul className='p-6 pt-16 overflow-y-auto w-80 bg-base-100 flex-col '>
-            <button type="submit" className="btn">Button</button>
-          </ul>
+      <>
+        <div className="btn btn-primary" onClick={clickHandler}>
+          open menu
         </div>
-      </div>
-    </>
+        <div className={`rounded-lg drawer drawer-end fixed h-screen w-full ${fullPage} `}>
+          <input id="my-drawer" type="checkbox" onChange={() => console.log("")} className="drawer-toggle"
+                 checked={isOpen}/>
+          <div className="drawer-side">
+            <label form="my-drawer" className="drawer-overlay" onClick={clickHandler}></label>
+            <ul className='p-6 pt-16 overflow-y-auto w-80 bg-base-100 flex-col '>
+              <button type="submit" className="btn">Button</button>
+            </ul>
+          </div>
+        </div>
+      </>
   );
 
 }
