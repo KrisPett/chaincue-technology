aba6cc5a0959c07117b61a76ef7721703298c8fe
KQT3 <70701718+KQT3@users.noreply.github.com>
2025-06-20 06:09:03 +0200

    refactor: update footer and header components to remove projects section and unused icons

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,13 +7,7 @@ const Footer = () => {
   return (
     <footer className="footer p-5 flex justify-center border-t-2 border-zinc-300 dark:border-zinc-600">
       <div className={"w-4/6 flex xxs:flex-col sm:flex-row justify-evenly items-center"}>
-        <div className={"flex flex-row w-full justify-evenly"}>
-          <div className={"flex flex-col"}>
-            <span className="footer-title">Projects</span>
-            <Link href={links.chain_music} className="link link-hover">Chain Music</Link>
-            <Link href={links.chaincue_real_estate} className="link link-hover">Chaincue Real Estate</Link>
-            <Link href={links.chainbot} className="link link-hover">AI | Chainbot App</Link>
-          </div>
+        <div className={"flex flex-row w-full"}>
           <div className={"flex flex-col"}>
             <span className="footer-title">Company</span>
             <Link href={"contact"} className="link link-hover">Contact</Link>
@@ -23,10 +17,8 @@ const Footer = () => {
           <span className="footer-title">Social</span>
           <div className={"mb-3 mt-3 flex xxs:space-x-2 xs:space-x-4"}>
             <IconHeader kind={"github"} href={links.github} tooltip={"Github"} _blank={"_blank"}/>
-            <IconHeader kind={"gitlab"} href={links.gitlab} tooltip={"Gitlab"} _blank={"_blank"}/>
             <IconHeader kind={"x"} href={links.x} tooltip={"X"} _blank={"_blank"}/>
             <IconHeader kind={"youtube"} href={links.youtube} tooltip={"Youtube"} _blank={"_blank"}/>
-            <IconHeader kind={"soundcloud"} href={links.soundcloud} tooltip={"Soundcloud"} _blank={"_blank"}/>
           </div>
         </div>
       </div>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -39,43 +39,8 @@ const Header = () => {
           </div>
           <div>
             <ul className={"flex space-x-1"}>
-              <div className="navbar-center hidden lg:flex">
+              <div className="navbar-center">
                 <ul className=" flex px-1">
-                  <li>
-                    <div className="dropdown dropdown-hover dropdown-bottom">
-                      <label tabIndex={0}
-                             onClick={() => router.push("projects")}
-                             className="btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500">
-                        Projects
-                        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                             viewBox="0 0 24 24">
-                          <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/>
-                        </svg>
-                      </label>
-                      <ul tabIndex={0}
-                          className="dropdown-content menu rounded-lg p-4 bg-zinc-300 dark:bg-zinc-600 space-y-4 dark:bg-gradient-to-b from-zinc-600 to-gray-500">
-                        <li className={"mt-0"}>
-                          <Link href={links.chain_music}
-                                className={"xxs:w-40 xs:w-80 h-20 btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500"}>
-                            ChainMusic
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={links.chaincue_real_estate}
-                                className={"xxs:w-40 xs:w-80 h-20 btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500"}>
-                            Chaincue Real Estate
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href={links.chainbot}
-                                className={"xxs:w-40 xs:w-80 h-20 btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500"}>
-                            AI | ChainBot
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
-
                   <li><Link href="/contact"
                             className={"btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500"}>Contact</Link>
                   </li>
@@ -106,39 +71,11 @@ const Header = () => {
             <div className={" flex mr-2"}>
               <ThemeSwitch1/>
             </div>
-            <div className="dropdown dropdown-hover dropdown-end lg:hidden">
-              <label tabIndex={0}
-                     className="btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500">
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                     viewBox="0 0 24 24">
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/>
-                </svg>
-              </label>
-              <ul tabIndex={0}
-                  className="dropdown-content menu rounded-lg p-4 bg-zinc-100 dark:bg-zinc-600 space-y-4 dark:bg-gradient-to-b from-zinc-600 to-gray-500">
-                <li className={"mt-0"}><Link href={links.chain_music}
-                                             className={"xxs:w-40 xs:w-80 h-20 btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500"}>ChainMusic</Link>
-                </li>
-                <li><Link href={links.crypto_app}
-                          className={"xxs:w-40 xs:w-80 h-20 btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500"}>
-                  CryptoApp
-                </Link>
-                </li>
-                <li>
-                  <Link href={links.chainbot}
-                        className={"xxs:w-40 xs:w-80 h-20 btn btn-ghost text-gray-600 dark:text-gray-200 hover:text-orange-800 dark:hover:text-orange-500"}>
-                    AI | ChainBot
-                  </Link>
-                </li>
-              </ul>
-            </div>
           </div>
           <div className={"hidden xl:flex space-x-2"}>
             <IconHeader kind={"github"} href={links.github} tooltip={"Github"} _blank={"_blank"}/>
-            <IconHeader kind={"gitlab"} href={links.gitlab} tooltip={"Gitlab"} _blank={"_blank"}/>
             <IconHeader kind={"x"} href={links.x} tooltip={"X"} _blank={"_blank"}/>
             <IconHeader kind={"youtube"} href={links.youtube} tooltip={"Youtube"} _blank={"_blank"}/>
-            {/* <IconHeader kind={"soundcloud"} href={links.soundcloud} tooltip={"Soundcloud"} _blank={"_blank"}/> */}
           </div>
         </div>
       </div>
